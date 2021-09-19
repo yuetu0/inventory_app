fb47b6f28f0b529239d88f1c8251be67ccfff72d
yuetu0 <gaoqinya05@gmail.com>
2021-09-18 18:40:38 -0700

    comment

--- inventory_app-main/lib/controller/form_controller.dart
+++ inventory_app-main/lib/controller/form_controller.dart
@@ -1,28 +1,30 @@
+//comment
 import 'dart:convert' as convert;
 import 'package:http/http.dart' as http;
 import '../model/form.dart';
 
-/// FormController is a class which does work of saving FeedbackForm in Google Sheets using 
+/// FormController is a class which does work of saving FeedbackForm in Google Sheets using
 /// HTTP GET request on Google App Script Web URL and parses response and sends result callback.
 class FormController {
-  
   // Google App Script Web URL.
-  static const String URL = "https://script.google.com/macros/s/AKfycbzqYAGjj5tD1ZUhPdugDR6-dCiQ16iFA7lo66JXW9esSgTa57Pv/exec";
-  
+  static const String URL =
+      "https://script.google.com/macros/s/AKfycbzqYAGjj5tD1ZUhPdugDR6-dCiQ16iFA7lo66JXW9esSgTa57Pv/exec";
+
   // Success Status Message
   static const STATUS_SUCCESS = "SUCCESS";
 
-  
-   void submitForm(
+  void submitForm(
       FeedbackForm feedbackForm, void Function(String) callback) async {
     try {
-      await http.post(Uri.parse(URL), body: feedbackForm.toJson()).then((response) async {
+      await http
+          .post(Uri.parse(URL), body: feedbackForm.toJson())
+          .then((response) async {
         if (response.statusCode == 302) {
           var url = response.headers['location'];
-          await http.get(Uri.parse(url),headers: {
-    'Content-Type': 'application/json',
-    'Charset': 'utf-8',
-  }).then((response) {
+          await http.get(Uri.parse(url), headers: {
+            'Content-Type': 'application/json',
+            'Charset': 'utf-8',
+          }).then((response) {
             callback(convert.jsonDecode(response.body)['status']);
           });
         } else {
