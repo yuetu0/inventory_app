1a20a7d641750b1638174eb7d82aa200d78917e0
yuetu0 <gaoqinya05@gmail.com>
2021-10-13 17:37:09 -0400

    redone

---
+++ lib/page/create_sheets_page.dart
@@ -0,0 +1,30 @@
+import 'dart:html';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:inventory_app/api/sheets/item_sheets_api.dart';
+import 'package:inventory_app/model/item.dart';
+import '/widget/button_widget.dart';
+import 'package:gsheets/src/gsheets.dart';
+import 'package:gsheets/gsheets.dart';
+
+class CreateSheetsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => Scaffold(
+      appBar: AppBar(
+        title: Text("Inventory App"),
+        centerTitle: true,
+      ),
+      body: Container(
+          alignment: Alignment.center,
+          padding: EdgeInsets.all(32),
+          child: ButtonWidget(
+            text: 'Save',
+            onClicked: () async {
+              final item = {
+                ItemFields.amount = 'hammer',
+              };
+              await ItemSheetsApi.insert([user]);
+            },
+          )));
+}
